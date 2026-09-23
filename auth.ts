8563340203f7039dc66c1ebfb6fab30c9019185a
Dalete <xdalete@proton.me>
2026-09-23 14:36:38 -0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -2,5 +2,7 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
+  debug: true,
   providers: [GitHub]
 });
