6f792803987efb66148cf7282f4393d11a4db56d
Dalete <xdalete@proton.me>
2026-09-23 16:29:37 -0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -3,11 +3,5 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
-  debug: true,
-  providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID!,
-      clientSecret: process.env.AUTH_GITHUB_SECRET!
-    })
-  ]
+  providers: [GitHub]
 });
